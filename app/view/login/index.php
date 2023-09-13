c0f2cae4771b22c40ac2d3bea4f0fbd345d504ba
Ludwig Bethnicer Napigkit <alphaphpn@gmail.com>
2023-09-13 21:38:36 +0800

    fb login

---
+++ app/view/login/index.php
@@ -0,0 +1,13 @@
+<?php
+	// code here
+	// https://www.facebook.com/account_status/?location=profile_settings
+?>
+
+	<section>
+		<div class="container">
+			<div id="fb-root"></div>
+			<script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v18.0&appId=855796602827996&autoLogAppEvents=1" nonce="fL4W87vy"></script>
+
+			<div class="fb-login-button" data-width="" data-size="" data-button-type="" data-layout="" data-auto-logout-link="false" data-use-continue-as="true"></div>
+		</div>
+	</section>
---
+++ routes/login/index.php
@@ -0,0 +1,8 @@
+<?php
+
+	include_once "../../lib/core.php";
+	include_once "../../lib/webconfig.php";
+	$page_title = "Login";
+	include_once "../../app/theme/default/header.php";
+	include_once "../../app/view/login/index.php";
+	include_once "../../app/theme/default/footer.php";
