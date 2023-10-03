44bde64b41196ea0a172fb9ccfc0a67a3e5d23df
Ludwig Bethnicer Napigkit <alphaphpn@gmail.com>
2023-10-03 16:48:21 +0800

    added meta UTF-8 at header

--- app/theme/default/header.php
+++ app/theme/default/header.php
@@ -2,6 +2,7 @@
 <html lang="en">
 <head>
 	<title><?php if (empty($page_title)) {echo trim($sysname);} else {echo trim($sysname.": ".$page_title);} ?></title>
+	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<meta name="description" content="">
 	<meta name="author" content="">
