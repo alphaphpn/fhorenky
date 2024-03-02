12aed859b6021fd2efac4f6b15453e968eb5e6ee
AlphaPhpN <101648773+alphaphpn@users.noreply.github.com>
2024-03-02 18:05:06 +0800

    added 2 DTR

--- app/theme/default/dashpanel/sidenav-core.php
+++ app/theme/default/dashpanel/sidenav-core.php
@@ -2,27 +2,27 @@
 							if ($_SESSION["ulevel"]==1) {
 						?>
 							<a class="nav-link" href="../../routes/#">
-								<div class="sb-nav-link-icon"><i class="fas fa-desktop"></i></div>
+								<div class="sb-nav-link-icon"><i class="fas fa-cogs"></i></div>
 								Settings
 							</a>
 
 							<a class="nav-link" href="../../routes/#">
-								<div class="sb-nav-link-icon"><i class="fas fa-desktop"></i></div>
+								<div class="sb-nav-link-icon"><i class="fas fa-users"></i></div>
 								User(s)
 							</a>
 
 							<a class="nav-link" href="../../routes/#">
-								<div class="sb-nav-link-icon"><i class="fas fa-desktop"></i></div>
+								<div class="sb-nav-link-icon"><i class="fas fa-palette"></i></div>
 								Appearance
 							</a>
 
 							<a class="nav-link" href="../../routes/#">
-								<div class="sb-nav-link-icon"><i class="fas fa-desktop"></i></div>
+								<div class="sb-nav-link-icon"><i class="fas fa-sticky-note"></i></div>
 								Site
 							</a>
 
 							<a class="nav-link" href="../../routes/#">
-								<div class="sb-nav-link-icon"><i class="fas fa-desktop"></i></div>
+								<div class="sb-nav-link-icon"><i class="fa fa-inbox"></i></div>
 								Contact(s)
 							</a>
 						<?php
